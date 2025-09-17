a14f56610a1fa92c354a6ceb9521570340f6c2bf
keerui <178910386+krsng-lab@users.noreply.github.com>
2025-09-17 08:37:53 +0800

    Create function to return value randomly
    getComputerChoice() function randomly returns string value of either "rock",
    "paper" or "scissors".

---
+++ script.js
@@ -0,0 +1,18 @@
+// Declare function getComputerChoice()
+    // Compute random value of either "rock", "paper" or "scissors"
+    // Return the value
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * 100);
+    if (index <= 100/3) {
+        return "rock";
+        console.log("rock");
+    }
+    else if (index >= 200/3) {
+        return "paper";
+        console.log("paper");
+    }
+    else {
+        return "scissors";
+        console.log("scissors");
+    }
+}
