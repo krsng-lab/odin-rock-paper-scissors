e90fa4d32eb967ac524fd0a8ab85e88f7e3d1e6b
keerui <178910386+krsng-lab@users.noreply.github.com>
2025-09-17 09:01:42 +0800

    Create variables to keep track of scores

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+// Initialise variables to keep track of scores
+let humanScore = 0;
+let computerScore = 0; 
+
 // Declare function getComputerChoice()
     // Compute random value of either "rock", "paper" or "scissors"
     // Return the value
-// Declare function getHumanChoice()
-    // Prompt user for choice
-        // Assume user inputs valid choice
-    // Initialise a constant 'choice'
-
 function getComputerChoice() {
     const index = Math.floor(Math.random() * 100);
     if (index <= 100/3) {
@@ -18,6 +17,10 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+// Declare function getHumanChoice()
+    // Prompt user for choice
+        // Assume user inputs valid choice
+    // Initialise a constant 'choice'
 function getHumanChoice() {
     let choice = prompt("Choose either 'rock', 'paper' or 'scissors'.");
 }
