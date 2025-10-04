fbf942c2450081569a6ebaa494d720a3fe4d7c20
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2025-10-04 16:37:56 +0800

    Add event listener to buttons to play a round

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// Create and initialise variables to keep track of scores
-let humanScore = 0;
-let computerScore = 0; 
-// Declare function getComputerChoice()
-    // Compute random value of either "rock", "paper" or "scissors"
-    // Return the value
-function getComputerChoice() {
-    const index = Math.floor(Math.random() * 100);
-    if (index <= 100/3) {
-        return "rock";
-    }
-    else if (index >= 200/3) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-// Declare function getHumanChoice()
-    // Prompt user for choice
-        // Assume user inputs valid choice
-    // Initialise a constant 'choice'
-function getHumanChoice() {
-    return choice = prompt("Choose either 'rock', 'paper' or 'scissors'.");
-}
+// // Create and initialise variables to keep track of scores
+// let humanScore = 0;
+// let computerScore = 0; 
+// // Declare function getComputerChoice()
+//     // Compute random value of either "rock", "paper" or "scissors"
+//     // Return the value
+// function getComputerChoice() {
+//     const index = Math.floor(Math.random() * 100);
+//     if (index <= 100/3) {
+//         return "rock";
+//     }
+//     else if (index >= 200/3) {
+//         return "paper";
+//     }
+//     else {
+//         return "scissors";
+//     }
+// }
+// // Declare function getHumanChoice()
+//     // Prompt user for choice
+//         // Assume user inputs valid choice
+//     // Initialise a constant 'choice'
+// function getHumanChoice() {
+//     return choice = prompt("Choose either 'rock', 'paper' or 'scissors'.");
+// }
 // Declare function playGame() that starts the game
     // Declare function playRound() that plays a single round
         // Read humanChoice and computerChoice
@@ -37,21 +37,34 @@ function getHumanChoice() {
             // Display message
             // Increment human score
         // Endif
-function playGame() {
-    function playRound(originalHumanChoice, originalComputerChoice) {
-        let humanChoice = originalHumanChoice.toLowerCase();
-        let computerChoice = originalComputerChoice.toLowerCase();
-        if (humanChoice == "rock" && computerChoice == "paper" || 
-            humanChoice == "paper" && computerChoice == "scissors" || 
-            humanChoice == "scissors" && computerChoice == "rock") {
-            console.log(`You lose! As ${computerChoice} beats ${humanChoice}.`);
-            computerChoice++;
+// function playGame() {
+//     function playRound(originalHumanChoice, originalComputerChoice) {
+//         let humanChoice = originalHumanChoice.toLowerCase();
+//         let computerChoice = originalComputerChoice.toLowerCase();
+//         if (humanChoice == "rock" && computerChoice == "paper" || 
+//             humanChoice == "paper" && computerChoice == "scissors" || 
+//             humanChoice == "scissors" && computerChoice == "rock") {
+//             console.log(`You lose! As ${computerChoice} beats ${humanChoice}.`);
+//             computerChoice++;
+//         }
+//         else if (humanChoice == computerChoice) {
+//             console.log("Tie!");
+//         }
+//         else {
+//             console.log(`You win! As ${humanChoice} beats ${computerChoice}.`);
+//             humanChoice++;
+//         }
+// }}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.id = "rock") {
+            playRound();
         }
-        else if (humanChoice == computerChoice) {
-            console.log("Tie!");
+        else if (button.id ="paper") {
+            playRound();
         }
-        else {
-            console.log(`You win! As ${humanChoice} beats ${computerChoice}.`);
-            humanChoice++;
-        }
-}}
+        else playRound();
+    });
+});
