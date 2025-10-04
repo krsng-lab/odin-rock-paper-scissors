3aef6fd3d612d59d59423d8dffae287390738099
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2025-10-04 16:51:11 +0800

    Remove function that gets player input via text

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// // Create and initialise variables to keep track of scores
-// let humanScore = 0;
-// let computerScore = 0; 
-// // Declare function getComputerChoice()
-//     // Compute random value of either "rock", "paper" or "scissors"
-//     // Return the value
-// function getComputerChoice() {
-//     const index = Math.floor(Math.random() * 100);
-//     if (index <= 100/3) {
-//         return "rock";
-//     }
-//     else if (index >= 200/3) {
-//         return "paper";
-//     }
-//     else {
-//         return "scissors";
-//     }
-// }
-// // Declare function getHumanChoice()
-//     // Prompt user for choice
-//         // Assume user inputs valid choice
-//     // Initialise a constant 'choice'
-// function getHumanChoice() {
-//     return choice = prompt("Choose either 'rock', 'paper' or 'scissors'.");
-// }
+// Create and initialise variables to keep track of scores
+let humanScore = 0;
+let computerScore = 0; 
+
+//Declare function getComputerChoice()
+    // Compute random value of either "rock", "paper" or "scissors"
+    // Return the value
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * 100);
+    if (index <= 100/3) {
+        return "rock";
+    }
+    else if (index >= 200/3) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
 // Declare function playGame() that starts the game
     // Declare function playRound() that plays a single round
         // Read humanChoice and computerChoice
